f82f2e444162a76e55112638c61b45bd227d51d4
Pony <pony@ponycool.com>
2022-10-29 15:05:56 +0800

    update pageviews plugin

--- PageViews/Plugin.php
+++ PageViews/Plugin.php
@@ -1,55 +1,70 @@
 <?php
 
+use Utils\Helper;
+use Typecho\Plugin\PluginInterface;
+use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Text;
+use Typecho\Widget\Helper\Form\Element\Radio;
+use Typecho\Plugin\Exception;
+
 /**
  * 文章浏览量统计
  *
  * @package PageViews
  * @author Pony
- * @version 1.0.1
- * @link http://blog.ponycool.com
+ * @version 1.2.0
+ * @link https://mayanpeng.cn
  */
-class PageViews_Plugin implements Typecho_Plugin_Interface
+class PageViews_Plugin implements PluginInterface
 {
-    protected static $pluginName = "PageViews";
+    protected static string $pluginName = "PageViews";
 
     /* 激活插件方法 */
-    public static function activate()
+    public static function activate(): string
     {
         Typecho_Plugin::factory('Widget_Archive')->beforeRender = array('PageViews_Plugin', 'statisticalPageViews');
         return '插件安装成功，请进入设置填写起始浏览量';
     }
 
     /* 禁用插件方法 */
-    public static function deactivate()
+    public static function deactivate(): string
     {
         return '插件卸载成功';
     }
 
     /* 插件配置方法 */
-    public static function config(Typecho_Widget_Helper_Form $form)
+    public static function config(Form $form)
     {
-        $views = new Typecho_Widget_Helper_Form_Element_Text("views", null, '0', '浏览量：', '文章起始浏览量，默认为0');
+        $prefix = new Text("prefix", null, '本站总访问量', '显示前缀：', '显示前缀');
+        $suffix = new Text("suffix", null, '次', '统计单位：', '统计单位，默认为次');
+        $views = new Text("views", null, '0', '浏览量：', '文章起始浏览量，默认为0');
+        $isHumanize = new Radio("isHumanize", ['禁用', '开启'], 0, '易读模式：', '易读模式，使用（K、M、B）进行计数');
+        $form->addInput($prefix);
+        $form->addInput($suffix);
         $form->addInput($views);
-        // TODO: Implement config() method.
+        $form->addInput($isHumanize);
     }
 
     /* 个人用户的配置方法 */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    public static function personalConfig(Form $form)
     {
         // TODO: Implement personalConfig() method.
     }
 
     /**
      * 统计浏览量
-     * @throws Typecho_Db_Exception
-     * @throws Typecho_Exception
+     * @throws Exception
      * @author Pony
      */
     public static function statisticalPageViews()
     {
         if (Typecho_Widget::widget('Widget_Archive')->is('single')) {
             $options = Helper::options();
-            $pluginConfig = $options->plugin(self::$pluginName);
+            try {
+                $pluginConfig = $options->plugin(self::$pluginName);
+            } catch (Exception $exc) {
+                throw new Exception('PageViews插件启用失败，错误代码：' . $exc->getCode());
+            }
             $views = $pluginConfig->views;
             $views++;
             //更新浏览量
@@ -60,22 +75,44 @@ class PageViews_Plugin implements Typecho_Plugin_Interface
     /**
      * 显示浏览量
      * 语法: PageViews_Plugin::showPageViews();
-     * 输出：'本站总访问量 XX 次'
-     * 语法: PageViews_Plugin::showPageViews('点击量','次');
-     * 输出：'点击量 XX 次'
-     *
-     * @param string $before
-     * @param string $after
-     * @throws Typecho_Db_Exception
+     * @throws Exception
      * @author Pony
      */
-    public static function showPageViews($before = '本站总访问量 ', $after = ' 次')
+    public static function showPageViews()
     {
         $options = Helper::options();
-        $pluginConfig = $options->plugin(self::$pluginName);
+        try {
+            $pluginConfig = $options->plugin(self::$pluginName);
+        } catch (Exception $exc) {
+            throw new Exception('PageViews插件显示浏览量异常，错误代码：' . $exc->getCode());
+        }
         $views = $pluginConfig->views;
+        $prefix = $pluginConfig->prefix;
+        $suffix = $pluginConfig->suffix;
+        $isHumanize = $pluginConfig->isHumanize;
         $views++;
-        $res = $before . $views . $after;
+        if ($isHumanize === '1') {
+            $views = self::humanize($views);
+        }
+        $res = sprintf('%s %s', $prefix, $views);
+        if ($suffix !== null) {
+            $res = sprintf('%s %s', $res, $suffix);
+        }
         echo $res;
+    }
+
+    /**
+     * 转换为人类易读格式
+     * @param int $views
+     * @return string
+     */
+    public static function humanize(int $views): string
+    {
+        return match (true) {
+            $views > 100000000 => sprintf('%s.0fB+', $views / 100000000),
+            $views > 1000000 => sprintf('%s.0fM+', $views / 1000000),
+            $views > 1000 => sprintf('%.0fK+', $views / 1000),
+            default => sprintf('%s', $views),
+        };
     }
 }
--- PageViews/README.md
+++ PageViews/README.md
@@ -1,4 +1,4 @@
-## Typecho 文章浏览量统计插件 PageViews v1.0.0
+## Typecho 文章浏览量统计插件 PageViews v1.2.0
 ---
 统计站点内容页面的总浏览量，所谓内容页面，包括文章页、独立页面和附件显示页
 
@@ -6,22 +6,25 @@
 
 1. 下载本插件，放在 usr/plugins/ 目录中
 2. 登录管理后台，激活插件
-3. 在显示的地方调用显示方法
+3. 在需要显示的地方调用显示方法
+
 ```php
 语法: PageViews_Plugin::showPageViews();
 输出: '本站总访问量 XX 次'
-
-语法: PageViews_Plugin::showPageViews('点击量','次');
-输出: '点击量 XX 次'
 ```
-**Logs**
 
+### Logs
+
+- 1.2.0
+    - 修改配置方式为后台插件配置
+    - 适配PHP8
+    - 新增人类易读模式
 - 1.0.1
-  - 增加起始浏览量
-  - 简化逻辑
-   
-**Links**
+    - 增加起始浏览量
+    - 简化逻辑
 
-- Blog：http://blog.ponycool.com 
+### Links
+
+- Blog：https://mayanpeng.cn
 - Email: pony#ponycool.com(将#替换为@)
 - Github: https://github.com/PonyCool
